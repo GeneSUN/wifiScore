e213ea699cd08dcf36601552b16373d5b910413c
Zhe Sun <injure21@gmail.com>
2024-02-20 09:28:01 -0500

    Update extender_installation_analysis.py

--- extender_installation_analysis.py
+++ extender_installation_analysis.py
@@ -1,9 +1,10 @@
 from pyspark.sql import functions as F 
-from pyspark.sql.functions import (concat,from_unixtime,lpad, broadcast, sum, udf, col, abs, length, min, max, lit, avg, when, concat_ws, to_date, exp, explode,countDistinct, first,round  ) 
+from pyspark.sql.functions import (collect_list,concat,from_unixtime,lpad, broadcast, sum, udf, col, abs, length, min, max, lit, avg, when, concat_ws, to_date, exp, explode,countDistinct, first,round  ) 
 from pyspark.sql import SparkSession 
+from pyspark.sql.types import FloatType
 from datetime import datetime, timedelta, date
 from dateutil.relativedelta import relativedelta 
-
+import numpy as np
 from dateutil.parser import parse
 import argparse 
 
@@ -31,7 +32,7 @@ def read_parquet_file_by_date(date, file_path_pattern):
         #print(file_path)
         return df
     except Exception as e:
-        print(e)
+        print("read_parquet_file_by_date",e,file_path)
 
 def process_parquet_files_for_date_range(date_range, file_path_pattern): 
 
@@ -48,21 +49,100 @@ class extenders_parquet():
     def __init__(self, 
             sparksession,
             install_extender_date,
-            window_range
-            
+            window_range,
+            columns_to_agg,
+            device_path,
+            home_path
         ) -> None:
         self.spark = sparksession
         self.install_extender_date = install_extender_date # e.g. 10
         self.window_range = window_range # e.g. 30
-
+        self.columns_to_agg = columns_to_agg
+        self.device_path = device_path
+        self.home_path = home_path
         self.before_extender_date = self.install_extender_date - timedelta( self.window_range )
         self.after_extender_date = self.install_extender_date + timedelta( self.window_range )
-        #self.device_path = hdfs_pd + "user/maggie/temp1.1/wifiScore_detail{}.json"
-        self.device_path = hdfs_pd + "/user/ZheS/wifi_score_v3/deviceScore_dataframe/{}"
+        
 
         self.df_ext_bef_aft = self.get_extender_bef_aft()
 
+    def serialNum_RouExt(self,start_date, window_range = None, device_path = None):
+        if window_range is None:
+            window_range = self.window_range
+        if device_path is None:
+            device_path = self.device_path
+
+        date_range = [ ( start_date + timedelta(i) ).strftime('%Y-%m-%d') for i in range(0, window_range)]
         
+        # label the device as extender-connected(Rou_Ext = 1) as long as one time connected to the extender
+        union_df = process_parquet_files_for_date_range(date_range, device_path)
+        result_df = union_df.groupBy("serial_num").agg( F.max("Rou_Ext").alias("Rou_Ext") )
+        
+        return result_df   
+ 
+    def agg_home(self,  date_range, df_extender, file_path_pattern = None, columns_to_agg = None,  id_columns = None ):   
+        if file_path_pattern is None:
+            file_path_pattern = self.home_path 
+        if columns_to_agg is None:
+            columns_to_agg = ["home_score"] 
+        if id_columns is None:
+            id_columns = ["serial_num","home_score"]
+        
+        df_list = [] 
+        for d in date_range: 
+            file_path = file_path_pattern.format( d )
+            try:
+                df_kpis = spark.read.parquet(file_path).select(id_columns).join( broadcast(df_extender), "serial_num" )
+                df_list.append(df_kpis)
+                print(file_path)
+            except Exception as e:
+                print("agg_home",e,file_path)   
+        
+        home_ext = reduce(lambda df1, df2: df1.unionAll(df2), df_list)
+        result_df = home_ext.groupBy("serial_num").agg( avg("home_score").alias("home_score"))
+        
+        return result_df
+        
+    def agg_device(self, date_range, df_extender, file_path_pattern = None, columns_to_agg = None,  id_columns = None):
+        if file_path_pattern is None:
+            file_path_pattern = self.device_path
+        if columns_to_agg is None:
+            columns_to_agg = self.columns_to_agg
+            # columns_to_agg = ["avg_phyrate", "poor_phyrate", "poor_rssi", "device_score", "weights",'stationarity',"volume"] 
+        if id_columns is None:
+            id_columns = columns_to_agg + ['Rou_Ext', 'cust_id', 'date', 'dg_model', 'firmware', 'mdn', 'rk_row_sn', 'rowkey',  'serial_num', 'station_mac']
+        df_list = [] 
+        for d in date_range: 
+            file_path = file_path_pattern.format( d )
+            try:
+                df_kpis = spark.read.parquet(file_path).select(id_columns)
+                df_list.append(df_kpis)
+                #print(file_path)
+            except Exception as e:
+                print("agg_device",e,file_path)     
+        Extenders = reduce(lambda df1, df2: df1.unionAll(df2), df_list).join( broadcast(df_extender), "serial_num" )
+        
+        average_columns = [F.avg(col).alias(col) for col in columns_to_agg]
+        median_columns = [ F.expr(f"percentile_approx({col}, {0.5})").alias(f"median_{col}") 
+                            for col in columns_to_agg if col != "device_score"] 
+    
+        result_df = Extenders.groupBy("serial_num")\
+                                    .agg(*average_columns, 
+                                            *median_columns, 
+                                            collect_list("device_score").alias("device_scores_list"),
+                                            F.count("*").alias("count")) 
+        #------------------------------------------------------------------------
+        @udf(FloatType())
+        def get_lowest_top_3(scores_list): 
+            numbers_list = sorted(scores_list)[:3] 
+            return float( np.sum(numbers_list) / len(numbers_list) )
+
+        result_df = result_df.withColumn("lowest_3_scores", get_lowest_top_3("device_scores_list"))\
+                                .drop("device_scores_list")
+        #------------------------------------------------------------------------
+        numeric_columns = [e for e in result_df.columns if e != "serial_num"]
+        return round_numeric_columns(result_df, decimal_places= 4, numeric_columns = numeric_columns )
+    
     def get_extender_bef_aft(self, before_extender_date = None, install_extender_date = None, after_extender_date = None, date_window = None):
         
         if before_extender_date is None:
@@ -85,11 +165,16 @@ class extenders_parquet():
         # 2. join before extender features and after extender features (exclude install extender month)
         after_range = [ ( after_extender_date + timedelta(i) ).strftime('%Y-%m-%d') for i in range(date_window) ]
         after_install_features = self.agg_device(after_range, df_extender)\
-                                .select( "serial_num", col("device_score").alias("target_avg_device_score") )
+                                        .select( "serial_num", 
+                                                col("device_score").alias("target_avg_device_score"),
+                                                col("lowest_3_scores").alias("target_lowest_3_scores"),
+                                                )
         
         before_range = [ ( before_extender_date + timedelta(i) ).strftime('%Y-%m-%d') for i in range(date_window) ]
         before_install_features = self.agg_device(before_range,  df_extender)\
-                                        .withColumnRenamed("device_score","avg_device_score")
+                                        .withColumnRenamed("device_score","avg_device_score")\
+                                        .withColumnRenamed("lowest_3_scores","before_lowest_3_scores")\
+                                        
 
         before_install_homescore = self.agg_home(before_range,  df_extender).withColumnRenamed("home_score","before_home_score")
         after_install_homescore = self.agg_home(after_range,  df_extender).withColumnRenamed("home_score","after_home_score")
@@ -100,103 +185,57 @@ class extenders_parquet():
         
         return df_ext_bef_aft
         
-    def agg_home(self,  date_range, df_extender, file_path_pattern = None, columns_to_agg = None,  id_columns = None ):   
-        if file_path_pattern is None:
-            file_path_pattern = hdfs_pd + "/user/ZheS/wifi_score_v3/homeScore_dataframe/{}"
-        if columns_to_agg is None:
-            columns_to_agg = ["home_score"] 
-        if id_columns is None:
-            id_columns = ["serial_num","home_score"]
-        
-        df_list = [] 
-        for d in date_range: 
-            file_path = file_path_pattern.format( d )
-            try:
-                df_kpis = spark.read.parquet(file_path).select(id_columns).join( broadcast(df_extender), "serial_num" )
-                df_list.append(df_kpis)
-                print(file_path)
-            except Exception as e:
-                print(e)   
-        
-        home_ext = reduce(lambda df1, df2: df1.unionAll(df2), df_list)
-        result_df = home_ext.groupBy("serial_num").agg( avg("home_score").alias("home_score"))
-        
-        return result_df
-        
-    def agg_device(self, date_range, df_extender, file_path_pattern = None, columns_to_agg = None,  id_columns = None):
-        if file_path_pattern is None:
-            file_path_pattern = self.device_path
-        if columns_to_agg is None:
-            columns_to_agg = ["avg_phyrate", "poor_phyrate", "poor_rssi", "device_score", "weights",'stationarity'] 
-        if id_columns is None:
-            id_columns = ['Rou_Ext', 'avg_phyrate', 'cust_id', 'date', 'dg_model', 'firmware', 'mdn', 'poor_phyrate', 'poor_rssi', 'rk_row_sn', 'rowkey', 'device_score', 'serial_num', 'station_mac', 'stationarity', 'weights']
-
-        df_list = [] 
-        for d in date_range: 
-            file_path = file_path_pattern.format( d )
-            try:
-                df_kpis = spark.read.parquet(file_path).select(id_columns)
-                df_list.append(df_kpis)
-                #print(file_path)
-            except Exception as e:
-                print(e)    
-        Extenders = reduce(lambda df1, df2: df1.unionAll(df2), df_list).join( broadcast(df_extender), "serial_num" )
-        
-        average_columns = [F.avg(col).alias(col) for col in columns_to_agg]
-        median_columns = [ F.expr(f"percentile_approx({col}, {0.5})").alias(f"median_{col}") 
-                            for col in columns_to_agg if col != "device_score"] 
-    
-        result_df = Extenders.groupBy("serial_num")\
-                                    .agg(*average_columns, 
-                                            *median_columns, 
-                                            F.count("*").alias("count")) 
-    
-        numeric_columns = [e for e in result_df.columns if e != "serial_num"]
-        return round_numeric_columns(result_df, decimal_places= 4, numeric_columns = numeric_columns )
-    
-    def serialNum_RouExt(self,start_date, window_range = None, device_path = None):
-        if window_range is None:
-            window_range = self.window_range
-        if device_path is None:
-            device_path = self.device_path
-
-        date_range = [ ( start_date + timedelta(i) ).strftime('%Y-%m-%d') for i in range(0, window_range)]
-        
-        # label the device as extender-connected(Rou_Ext = 1) as long as one time connected to the extender
-        union_df = process_parquet_files_for_date_range(date_range, device_path)
-        result_df = union_df.groupBy("serial_num").agg( F.max("Rou_Ext").alias("Rou_Ext") )
-        
-        return result_df
-        
 
         
     
 if __name__ == "__main__":
     # the only input is the date which is used to generate 'date_range'
-    spark = SparkSession.builder.appName('ZheS_wifiscore_preprocess')\
+    spark = SparkSession.builder.appName('ZheS_wifiscore_get_extender')\
                         .config("spark.sql.adapative.enabled","true")\
                         .getOrCreate()
     hdfs_pd = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000/'
 
     #--------------------------------------------------------------------------------
-    start_d = date(2024, 2, 13)
-    window_range = 1
+    start_d = date(2024, 2, 16)
+    window_range = 2
     #--------------------------------------------------------------------------------
     #for i in range(0,30,window_range):
     d = start_d #+ timedelta(i)
     print(d)
-    inst1 = extenders_parquet( sparksession = spark, install_extender_date = d, window_range = window_range)
-    #inst1.df_ext_bef_aft.show()
+    inst_v3 = extenders_parquet( sparksession = spark, 
+                            install_extender_date = d, 
+                            window_range = window_range,
+                            columns_to_agg = ["avg_phyrate", "poor_phyrate", "poor_rssi", "device_score", "weights",'stationarity',"volume"], 
+                            device_path = hdfs_pd + "/user/ZheS/wifi_score_v3/deviceScore_dataframe/{}",
+                            home_path = hdfs_pd + "/user/ZheS/wifi_score_v3/homeScore_dataframe/{}"
+                            )
 
     output_path = (
-                    hdfs_pd + "/user/ZheS/wifi_score_v3/training_dataset/" + \
-                    f"{inst1.before_extender_date.strftime('%Y-%m-%d')  }_" +\
-                    f"{inst1.install_extender_date.strftime('%Y-%m-%d')  }_" +\
-                    f"{inst1.after_extender_date.strftime('%Y-%m-%d')  }_" +\
-                    f"window_range_{inst1.window_range}"
+                hdfs_pd + "/user/ZheS/wifi_score_v3/training_dataset/" + \
+                f"{inst_v3.before_extender_date.strftime('%Y-%m-%d')  }_" +\
+                f"{inst_v3.install_extender_date.strftime('%Y-%m-%d')  }_" +\
+                f"{inst_v3.after_extender_date.strftime('%Y-%m-%d')  }_" +\
+                f"window_range_{inst_v3.window_range}"
+                )
+    inst_v3.df_ext_bef_aft.write.mode("overwrite").parquet(output_path)
+    """
+
+    inst_v2 = extenders_parquet( sparksession = spark, 
+                            install_extender_date = d, 
+                            window_range = window_range,
+                            columns_to_agg = ["avg_phyrate", "poor_phyrate", "poor_rssi", "device_score", "weights",'stationarity'], 
+                            device_path = hdfs_pd + "/user/ZheS/wifi_score_v2/deviceScore_dataframe/{}",
+                            home_path = hdfs_pd + "/user/ZheS/wifi_score_v2/homeScore_dataframe/{}"
+                            )
+    output_path = (
+                    hdfs_pd + "/user/ZheS/wifi_score_v2/training_dataset/" + \
+                    f"{inst_v2.before_extender_date.strftime('%Y-%m-%d')  }_" +\
+                    f"{inst_v2.install_extender_date.strftime('%Y-%m-%d')  }_" +\
+                    f"{inst_v2.after_extender_date.strftime('%Y-%m-%d')  }_" +\
+                    f"window_range_{inst_v2.window_range}"
                     )
-    inst1.df_ext_bef_aft.write.mode("overwrite").parquet(output_path)
+    inst_v2.df_ext_bef_aft.write.mode("overwrite").parquet(output_path)
 
 
-
+    """
 
